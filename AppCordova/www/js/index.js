a617fd394393e75f8fa58b1f1dae38e33c7e8488
desbaram <melanie.desbarats@gmail.com>
2021-11-29 11:16:58 +0100

    ajoutPages

--- AppCordova/www/js/index.js
+++ AppCordova/www/js/index.js
@@ -1,24 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -27,3 +6,30 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
+
+document.getElementById("btn").addEventListener("click", send);
+
+function send() {
+    var log = document.getElementById("login").value;
+    var mdp = document.getElementById("mdp").value;
+    document.getElementById("user").innerHTML = log;
+    if(log!=="" && mdp!==""){
+    document.getElementById('log').style.display = "none";
+    document.getElementById('choix').style.display = "block";
+    }
+    //send(login, mdp) envoie variables au serveur
+ }
+
+ document.getElementById("nGame").addEventListener("click", displayGame);
+
+ function displayGame() {
+    document.getElementById('choix').style.display = "none";
+    document.getElementById('board').style.display = "block";
+ }
+
+ document.getElementById("class").addEventListener("click", displayClass);
+
+ function displayClass() {
+    document.getElementById('choix').style.display = "none";
+    document.getElementById('classement').style.display = "block";
+ }
