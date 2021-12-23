db31eee5640402118e2742a462da097b5a3fa900
Pauline <pa.teoulle@gmail.com>
2021-12-23 19:05:17 +0100

    Ajout des utilisateurs dans la liste pour attendre une partie

--- AppCordova/www/js/index.js
+++ AppCordova/www/js/index.js
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 //Ouverture d'une websocket
 const ws = new WebSocket('ws://127.0.0.1:9898/');
 //Listener d'ouverture de websocket
@@ -25,20 +6,12 @@ ws.onopen = function() {
 
 };
 
-function displayClassement() {
-    document.getElementById("choix").style.display = "none";
-    document.getElementById('classement').style.display = "block";
+function handleNewGame() {
+    let json = JSON.stringify({ "message": "En attente d'une partie", "login": login, "mdp": mdp });
+    ws.send(json);
+    displayBoard();
 }
 
-function displayBoard() {
-    document.getElementById("choix").style.display = "none";
-    document.getElementById('board').style.display = "block";
-}
-
-function displayChoix() {
-    document.getElementById("log").style.display = "none";
-    document.getElementById("choix").style.display = "block";
-}
 /*
 Listener des messages entrants
 Selon le message du serveur que l'on vient de recevoir, on fait un affichage
@@ -50,40 +23,22 @@ ws.onmessage = function(e) {
         document.getElementById("messageServeur").style.color = "green";
         document.getElementById("messageServeur").innerHTML = e.data;
         console.log("Auth réussie");
-    }
-    //Cas où l'authentification est invalidée par le serveur
-    else if (e.data == "Authentification invalide - Serveur") {
-        document.getElementById("messageServeur").style.color = "red";
-
-        console.log("Auth non réussie");
-    }
-    //Cas pour remettre le style par défault pour un message simple
-    else if (e.data == "Message bien reçu - Serveur") {
-        document.getElementById("messageServeur").style.color = "white";
     } else if (e.data == "Utilisateur Connecté") {
         displayChoix();
     }
 };
 
-
+let login;
+let mdp;
 
 /*
 Récupération des inputs du client, formattage en JSON et envoi vers le serveur
 */
 function authentification() {
-    let login = document.getElementById("login").value;
-    let mdp = document.getElementById("mdp").value;
-    let json = JSON.stringify({ "login": login, "mdp": mdp });
-    ws.send(json);
-}
-
-function send() {
-    var log = document.getElementById("login").value;
-    var mdp = document.getElementById("mdp").value;
-    document.getElementById("user").innerHTML = log;
-    if (log !== "" && mdp !== "") {
-        document.getElementById('log').style.display = "none";
-        document.getElementById('choix').style.display = "block";
+    login = document.getElementById("login").value;
+    mdp = document.getElementById("mdp").value;
+    let json = JSON.stringify({ "message": "Auth", "login": login, "mdp": mdp });
+    if (login != "" && mdp != "") {
+        ws.send(json);
     }
-    //send(login, mdp) envoie variables au serveur
 }
